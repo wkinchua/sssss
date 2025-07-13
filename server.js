09cbff7674c633715aeed4db9f40d740b5eb4ccd
Wkin <125807080+wkinchua@users.noreply.github.com>
2025-07-13 19:14:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -425,7 +425,7 @@ app.delete("/api/promotions/:id", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
